a449d0709a65c8d2a0e48d3160c66d85411d0c6d
et0005 <et0005@uah.edu>
2018-03-17 17:27:48 +0000

    Adjusted file structure

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 #!/usr/bin/python
 
-import routeEngine.py
-import IR_control.py
+# ------------------------------------------------
+#
+#    Title : main.py
+#
+#    Authors : Ashley Condrey
+#              David Flanigan
+#              Nathan Romans
+#              Payton Parrott
+#              Elena Todorovska
+#
+#    Created On : 01/01/2018
+#
+#    Description : This program is the main driver. 
+#
+#    Notes : This will be run on a Raspberry Pi
+#    using a 12V battery and 4 DC Motors. The
+#    motors are driven with PWM using a L293D.
+#
+# ------------------------------------------------
 
-#read IR
-#given route find config
-#load config
-#plug into route engine
-#execute route via route engine
+# Dependent Files
+from pirateRobot import routeEngine
+from pirateRobot import IR_control
+
+# Turn on rPI
+# Read IR for alignment
+#    Display on 7seg for alignment
+# Wait for 1, 2, 3, GO
+#    Flip Switch
+#         Upon switch
+#              Read IR route
+#              Load IR route into class variables (for turns only)
+#              IR route gets plugged into route engine
+#         Execute route via route engine
 
 if IR_control.IR1.destA == 1:
     if IR_control.IR1.destB == 1:
@@ -16,4 +42,10 @@ if IR_control.IR1.destA == 1:
 
 else:
     print("Begin Route!\n")
+    print("Route is : ABC : ")
+    print IR_control.IR1.destA
+    print IR_control.IR1.destB
+    print IR_control.IR1.destC
+    print("\n")
+        
     routeEngine.begin(IR_control.IR1)
